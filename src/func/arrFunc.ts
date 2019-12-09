d66917a42906dfe5dc02d750afd8f5c770c2ea75
dhoonjang <dhoonjang@gmail.com>
2019-12-09 18:09:56 +0900

    fix: stack tree nav (Array => ImStack)

--- src/func/arrFunc.ts
+++ src/func/arrFunc.ts
@@ -1,3 +1,26 @@
 export const getArrayAverage = (arr: Array<number>) => {
   return arr.reduce((sum, current) => sum + current, 0) / arr.length;
 };
+
+export class ImStack<T> {
+  private data: T[] = [];
+
+  constructor(dataInput: T[]) {
+    this.data = dataInput;
+  }
+
+  push(item: T): ImStack<T> {
+    this.data.push(item);
+
+    return new ImStack<T>(this.data);
+  }
+
+  pop(): ImStack<T> {
+    this.data.pop();
+    return new ImStack<T>(this.data);
+  }
+
+  top(): T {
+    return this.data[this.data.length - 1];
+  }
+}
--- src/view/StackTreeView/index.tsx
+++ src/view/StackTreeView/index.tsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import StackTree from "../../model/StackTree/index";
 import StackNodeView from "./StackNodeView";
 import { StackNode } from "../../type/modelType";
+import { ImStack } from "../../func/arrFunc";
 
 const StackTreeView: React.FC = () => {
   const [stack, setStack] = useState<StackNode>(StackTree);
-  const [path, setPath] = useState<Array<StackNode>>([]);
+  const [path, setPath] = useState<ImStack<StackNode>>(
+    new ImStack<StackNode>([])
+  );
 
   const navUp = () => {
-    const parent = path[path.length - 1];
+    const parent = path.top();
 
     if (parent) {
-      const newPath = path.filter(p => p !== parent);
+      const newPath = path.pop();
       setPath(newPath);
       setStack(parent);
     }
@@ -21,7 +24,7 @@ const StackTreeView: React.FC = () => {
     const child = stack.children[index];
 
     if (child) {
-      const newPath = path.concat(stack);
+      const newPath = path.push(stack);
       setPath(newPath);
       setStack(child.stack);
     }
